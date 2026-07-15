f28e68620580e5f70d7bb5d4fa58623429600bc9
Textterrion <terry994566@gmail.com>
2026-07-15 22:03:31 +0300

    fix: correct default port number to 3000 in server configuration

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 import express from 'express';
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 
 app.listen(PORT, () => {
