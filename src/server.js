d93063c967a25bef2fa27ea81b3c3598968078ee
Textterrion <terry994566@gmail.com>
2026-07-21 18:25:41 +0300

    feat: implement CRUD operations for notes and set up MongoDB connection

--- src/server.js
+++ src/server.js
@@ -1,52 +1,28 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import pinoHttp from 'pino-http';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(pinoHttp());
+const PORT = process.env.PORT || 3030;
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
+app.use(notesRoutes);
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
-
-app.use((err, req, res, next) => {
-  const statusCode = err.status || 500;
-
-  res.status(statusCode).json({
-    message: err.message || 'Internal server error',
-    status: 'fail',
-    code: statusCode,
-  });
-});
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
 });
-
 export default app;
